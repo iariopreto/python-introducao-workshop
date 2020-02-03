814c50b306f33c9fe9b784519092f8c62349fd55
Edinei <edinei3A@hotmail.com>
2020-02-03 08:35:33 -0300

    Add python exemplos

---
+++ src/exemplos.py
@@ -0,0 +1,13 @@
+def multiplicacao(a: int, b: int) -> int:
+    """Multiplicação de 2 numeros
+    
+    Arguments:
+        a {int} -- Primeiro numero
+        b {int} -- Segundo numero
+    
+    Returns:
+        int -- Returno da multiplicação
+    """
+    return a * b
+
+multiplicacao(1, 2)
